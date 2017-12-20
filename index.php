53c7fae5e8e6d3b09bfbdf57f8b609e5801961ca
yuji.fujiwara <yuji.fujiwara@nijibox.co.jp>
2017-12-20 20:37:08 +0900

    不要な読み込み削除

--- index.php
+++ index.php
@@ -5,8 +5,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
     <title>Okiagari</title>
 
-    <script src="http://www.natural-science.or.jp/javascript/WebGL/glMatrix.js"></script>
-
     <script src="./libs/stats.min.js"></script>
     <script src="./libs/three.js"></script>
     <script src="./libs/TrackballControls.js"></script>
